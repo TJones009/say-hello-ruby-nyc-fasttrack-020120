967dd27c38524191de36d5b83ecb6b12cbf00bef
Trevor Jones <tjones009@yahoo.com>
2020-02-01 18:22:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,8 @@
 def say_hello(name)
-puts "Hello Kent Beck"
+puts "Hello Trevor"
 end
 
-def say_hello(name="Ruby Programmer!")
+def say_hello(name="Ruby Programmer")
   puts ("Hello #{name}!")
 end
+
